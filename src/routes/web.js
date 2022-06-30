10d45d3e1b3131fddda4c65893db13f10a8e9537
DauNH <daunh@sskpi.com>
2022-06-30 11:42:52 +0700

    ORM get list  api CSR

--- src/routes/web.js
+++ src/routes/web.js
@@ -1,19 +1,24 @@
 import express from "express";
 import homeController from "../controller/homeController";
-
+import cors from "cors";
+import userService from "../service/useService";
 const router = express.Router();
 
 const initWebRoutes = (app) => {
-  router.get("/", homeController.handlehelloWorld);
 
-  router.get("/user", homeController.handleUser);
+  router.get("/", cors(), homeController.handlehelloWorld);
+
+  router.get("/user", cors(), homeController.handleUser);
 
   router.post("/user/create-user", homeController.handleCreateNewUser);
 
   router.post("/user/delete-user/:id", homeController.handleDeleteUser);
 
   router.get("/user/update-user/:id", homeController.handleUpdateUser);
+
   router.post("/user/update-user", homeController.onEditUser);
+
+  app.get("/user", cors(), homeController.handleUser);
 
   return app.use("/", router);
 };
